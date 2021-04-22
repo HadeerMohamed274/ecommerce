12eb31ae5662e7d0a6e9cbde3fc2eb90c0ca0d42
Hadeer Mohamed <82791730+HadeerMohamed274@users.noreply.github.com>
2021-04-22 04:13:29 +0300

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ecommerceui2</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#3C096C;">
+  <a class="navbar-brand" href="#"><img src="WhatsApp Image 2021-04-16 at 2.20.53 PM.jpeg" width="38" height="38" alt=""> Happiness</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+   
+    <form class="form-inline my-2 mr-auto ml-auto my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <a class="btn btn-outline-light my-2 mr-2 my-sm-0" href="" type="submit">Cart</a>
+    <a class="btn btn-outline-light my-2  my-sm-0" href="" type="submit">Sign Out</a>
+  </div>
+</nav>
+</body>
+</html>
